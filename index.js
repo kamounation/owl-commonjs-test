0195a07517e463ec3f5e76570911045350e99bf5
Xlaez <utibeabasiekong2@gmail.com>
2023-01-02 00:26:52 +0100

    update: added new package version and issue is resolved

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-const owlFactory = require('owl-factory');
+const OwlFactory = require('owl-factory');
 
 class TestController {
 	constructor() {}
 
-	getMsg = owlFactory.catchAsync(async (req, res, next) => {
+	getMsg = OwlFactory.catchAsync(async (req, res, next) => {
 		res.send('welcome to this endpoint');
+	});
+	sendMsg = OwlFactory.catchAsync(async (req, res, next) => {
+		const { body } = req;
+		if (!body.name)
+			return next(
+				new OwlFactory.AppRes(
+					OwlFactory.httpStatus.BAD_REQUEST,
+					'error, provide name in json'
+				)
+			);
+
+		res.status(200).json(body);
 	});
 }
 
 class TestRoute {
 	path = '/test';
-	router = owlFactory.Router();
+	router = OwlFactory.Router();
 	controller = new TestController();
 	constructor() {
 		this.initializeRoutes();
 	}
 	initializeRoutes() {
 		this.router.get(`${this.path}`, this.controller.getMsg);
+		this.router.post(`${this.path}`, this.controller.sendMsg);
 	}
 }
 
-const server = new owlFactory([new TestRoute()]);
+const server = new OwlFactory([new TestRoute()], '9999');
 
 server.listen();
